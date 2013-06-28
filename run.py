8b3ce628898fa8c948d4186445ba949583a012e8
Michael Scharf <github@scharf.gr>
2013-06-28 02:40:11 +0200

    launch in debug mode without reloader so that pydev can debug it

--- run.py
+++ run.py
@@ -5,4 +5,4 @@ if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     host = os.environ.get('HOST', '0.0.0.0')
     app = annotateit.create_app()
-    app.run(port=port, host=host)
+    app.run(port=port, host=host, use_reloader=False)
